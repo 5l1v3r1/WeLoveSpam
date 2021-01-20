ff9bc111f67bf259661e09e29b32ff1f75806bb0
Ataberk Kurt <zeaqqs@gmail.com>
2021-01-21 02:37:12 +0300

    Proje dosyası ekle.

---
+++ WeLoveSpam!/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using WindowsInput.Native;
+using WindowsInput;
+
+namespace WeLoveSpam_
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            /*
+                                Değişkenlerin Başlangıcı.
+            */
+            string SpamMesaji;
+            int SpamSayisi;
+            int SpamAnlik = 0;
+            InputSimulator yolla = new InputSimulator();
+            /*
+                                Değişkenlerin Bitişi.
+            */
+
+
+            Console.WriteLine("Spamlanacak Mesajı Giriniz: ");
+            SpamMesaji = Console.ReadLine();
+
+            Console.WriteLine("Ne Kadar Spamlanacak? ");
+            SpamSayisi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Spam 3 saniye içinde başlayacak..");
+            Thread.Sleep(3000);
+            Console.WriteLine("Spam Başladı!");
+
+            while (SpamAnlik < SpamSayisi)
+            {
+                yolla.Keyboard.TextEntry(SpamMesaji);
+                yolla.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                yolla.Keyboard.Sleep(1000);
+                SpamAnlik += 1;
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
